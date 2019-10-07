7a4d41636735abb0311b446d63b872dab62f79e5
vi-li <1976763043@qq.com>
2019-10-07 11:31:23 +0800

    代码提交

---
+++ C0Compiler/src/Execute/Main.java
@@ -0,0 +1,17 @@
+package Execute;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.init();
+    }
+
+    private void init(){
+       /* Exe exe = new Exe();
+        exe.init();*/
+        Execute execute=new Execute();
+        execute.start();
+    }
+
+}
