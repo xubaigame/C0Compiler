d9fd3515c6ea70b4f0dbbb6dbfc7578c2058c450
Mumu <>
2020-12-25 21:21:46 +0800

    清空仓库

--- C0Compiler/src/Compiler/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package Compiler;
-
-public enum ErrorCode {
-    ArithmeticException,    //算数异常
-    InputParamErrException, //输入参数错误
-    VariableException,      //变量异常
-    FunctionException,      //函数异常
-    UnknowException         //未知错误
-}
